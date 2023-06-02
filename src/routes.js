cbec45d9014b855e1dd5eafa10083e2852a78b91
Gustavo Santos <z.gsantos20@gmail.com>
2023-06-02 23:41:50 +0000

    Add the base endpoints

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
-const { Router } = require("express");
+const { Router } = require('express');
 
-const ContactController = require('./app/controllers/ContactController')
+const ContactController = require('./app/controllers/ContactController');
 
 const router = new Router();
 
-router.get('/contacts', ContactController.index)
-router.get('/contacts/:id', ContactController.show)
+router.get('/contacts', ContactController.index);
+router.get('/contacts/:id', ContactController.show);
 
-router.delete('/contacts/:id', ContactController.delete)
+router.delete('/contacts/:id', ContactController.delete);
+
+router.post('/contacts', ContactController.store);
+router.put('/contacts/:id', ContactController.update);
 
 module.exports = router;
